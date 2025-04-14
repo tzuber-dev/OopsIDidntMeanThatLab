3b8c507d94bbd3b84d333d2db4f69f50c6019483
tzuber-dev <zt50481@mail.harpercollege.edu>
2025-04-13 22:41:23 -0500

    Update oopslab.h

--- oopslab.h
+++ oopslab.h
@@ -6,12 +6,12 @@ template <typename T>
 vector<T> remove_duplicate(vector<T> input) {
 vector<T>  noduplicate;
 for (int i = 0; i < input.size(); i++){
-bool found = false;
+bool discovered = false;
 
 for (int j = 0; j < noduplicate.size(); j++)
 {
 if(input[i]== noduplicate[j]){
-    found = true;
+    discovered = true;
     break;}
     
 }
